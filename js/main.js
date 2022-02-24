6901d12fccba67723a0cb1e8e808607880105583
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-02-24 01:32:22 +0100

    botonoes cargados con margenes

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@
 /**
  an = an-2 + an-1
  */
-let listaEnumerada = [
-                        "Guarda termino previo que es 0.",
+let listaEnumerada = [  "Guarda termino previo que es 0.",
                         "Guarda termino actual que es 1.",
                         "Guarda termino actual y previo en una lista.",
                         "Genera un nuevo número  sumando termino previo al actual.",
-                        "Remplaza el valor de termino previo por actual.",
-                        "Remplaza termino actual por el obtenido en el último cálculo.",
+                        "Reemplaza el valor de termino previo por actual.",
+                        "Reemplaza termino actual por el obtenido en el último cálculo.",
                         "Suma termino actual a sumatorio de términos.",
                         "Guarda termino actual en la lista",
                         "Cuando el termino sea 4000 suma todos los términos conseguidos.",
@@ -17,14 +16,14 @@ let listaEnumerada = [
 
                     ];
 const cargador = document.getElementById("cargador");
-let txt;
+let txt ="Mapa mental del programa";
 
 let an, anMenos1, anMenos2;
 let listaTerminos = [];
 
 
 listaEnumerada.forEach( function(item){
-    txt += `                     
+    txt += `                   
     <div class="form-check form-switch">
         <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
         <label class="form-check-label" for="flexSwitchCheckDefault">${item}</label>
