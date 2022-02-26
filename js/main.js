2ad6ba1f9f25358e1f8bab716eadeba44236464f
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-02-26 02:14:12 +0100

    cargamos resultado

--- js/main.js
+++ js/main.js
@@ -20,12 +20,13 @@ let txt ="<h2>Secuencia para resolver problema:</h2>";
 /***************variables para resolber Fibonacci**************/
 
 
-let an, 
+let txt_contador2 = " ",
+    an, 
     anMenos1 = 0 , 
     anMenos2 = 1 ,
     suma = anMenos2;
 let listaTerminos = [anMenos1 , anMenos2 ];
-
+let contenedor2 = document.getElementById("contenedor2");
 
 
 
@@ -58,11 +59,19 @@ for (let n = 2 ; n < 100000 ; n++){
     listaTerminos[n] = listaTerminos[n-2] + listaTerminos[n-1];
     if(listaTerminos[n] >   4000000)  break;
     if(listaTerminos[n] % 2 === 0 ) suma +=  listaTerminos[n]; 
-
+    txt_contador2 += ` ${listaTerminos[n]},  `
     }  
 
 
 /**********************************************************************************************************/
+contenedor2.innerHTML = `
+<h3>Resultado de la prueba</h3>
+<p>Dentro de este div regresamos los resultados generados en automático por js</p>
+<h4>Resultado de la suma de los términos pares</h4>
+<p style= "color: red"> ${suma}</p> 
+<h4>Terminos</h4> 
+<p style="color:blue">${txt_contador2}</p>
 
+`; 
 console.info(suma-1);
 console.info(listaTerminos);
